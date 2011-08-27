da39014475fe9158c40fd4c0396c10026786516c
fhahn <flo@fhahn.com>
2011-08-27 22:38:39 +0200

    added tests for delete and exists

--- gaeblob_storage/tests.py
+++ gaeblob_storage/tests.py
@@ -20,13 +20,29 @@ class HashPathStorageTest(TestCase):
         path = self.storage.save(self.name, self.file)
         self.assertEqual(self.name, path)
 
-    def test_open_file(self):
+    def test_open_existing_file(self):
         path = self.storage.save(self.name, self.file)
         self.assertEqual(self.name, path)
 
         f = self.storage.open(path)
         self.assertEqual(self.content, f.content)
-        
+
+    def test_open_invalid_file(self):
+        f = self.storage.open('foo bar')
+        self.assertEqual(f, None)
+
+    def test_exists_existing_file(self):
+        path = self.storage.save(self.name, self.file)
+        self.assertTrue(self.storage.exists(path))
+
+    def test_exists_invalid_file(self):
+        self.assertFalse(self.storage.exists('foo bar'))
+
+    def test_delete_existing_file(self):
+        self.test_exists_existing_file()
+        self.storage.delete(self.name)
+        self.assertFalse(self.storage.exists(self.name))
+
     def test_save_same_file(self):
         path_1 = self.storage.save(self.name, self.file)
         path_2 = self.storage.save(self.name, self.file)
